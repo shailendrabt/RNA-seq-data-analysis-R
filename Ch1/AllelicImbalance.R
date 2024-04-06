f46200034474a98b4e0475f8a6410c3a9b9f63ca
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-04-06 15:20:16 +0530

    Create AllelicImbalance.R

---
+++ Ch1/AllelicImbalance.R
@@ -0,0 +1,25 @@
+BiocManager::install("AllelicImbalance")
+library("AllelicImbalance")
+ins
+region_of_interest <- GRanges(seqnames = c("17"), ranges = IRanges(79478301, 79478361))
+
+bam_folder <- file.path(getwd(), "R_cook", allele_expression")
+
+reads <- impBamGAL(bam_folder, region_of_interest, verbose = FALSE)
+
+vcf_file <-file.path( getwd(), "R_cook", "allele_expression","ERP000101.vcf" )
+
+variant_positions <- granges(VariantAnnotation::readVcf(vcf_file), "hg19" )
+
+
+allele_counts <- getAlleleCounts(reads, variant_positions, verbose=FALSE)
+
+ase.vcf <- ASEsetFromCountList(rowRanges = variant_positions, allele_counts)
+
+reference_sequence <- file.path(getwd(), "R_cook","allele_expression", "hg19.chr17.subset.fa")
+
+
+ref(ase.vcf) <- refAllele(ase.vcf,fasta=reference_sequence)
+alt(ase.vcf) <- inferAltAllele(ase.vcf)
+
+binom.test(ase.vcf, n="*")
