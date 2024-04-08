213f116359bb0f14710e1da7ae7cecdf515a711b
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-04-08 15:19:18 +0530

    Add files via upload

---
+++ Ch1/pakages.R
@@ -0,0 +1,19 @@
+
+#first we need to install BiocManager
+install.packages("BiocManager")
+
+BiocManager::install("edgeR")
+BiocManager::install("csaw")
+BiocManager::install("bumphunter")
+BiocManager::install("AllelicImbalance")
+BiocManager::install("IRanges")
+BiocManager::install("Rsamtools")
+BiocManager::install("rtracklayer")
+BiocManager::install("sva")
+BiocManager::install("summarizedExperiment")
+install.packages("dplyr")
+install.packages("extRemes")
+install.packages("forcats")
+install.packages("magrittr")
+install.packages("powsimR")
+install.packages("readr")
