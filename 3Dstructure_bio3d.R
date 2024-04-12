63b438c9269da0de8c3436a74f2353f56b98c53c
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-04-12 11:32:10 +0530

    Delete 3Dstructure_bio3d.R

--- 3Dstructure_bio3d.R
+++
@@ -1,7 +0,0 @@
-library(bio3d)
-a <- read.pdb(file.path(getwd(), "R_cook", "Ch3" ,"1xwc.pdb"))
-b <- read.pdb(file.path(getwd(), "R_cook", "Ch3", "3trx.pdb"))
-
-pdbs <- pdbaln(list("1xwc"=a,"3trx"=b), fit=TRUE,exefile="msa")
-pymol(pdbs, exefile = "/Users/macleand/miniconda2/bin/pymol", type = "launch", as="cartoon")
-
