47f8e2afc7db6757f3a1ce3386a935f31b345538
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-09-17 12:25:43 +0530

    Delete Ch6-Proteomics_data_Analysis directory

--- Ch6-Proteomics_data_Analysis/mzR2.R
+++
@@ -1,9 +0,0 @@
-###load library and import the source data files
-library(mzR)
-mzxml_file <- file.path(getwd(),  "Ch6", "threonine_i2_e35_pH_tree.mzXML")
-mzdata <- openMSfile(mzxml_file)
-###extract the header and peak data
-header_info <- header(mzdata)
-peak_data_list <- spectra(mzdata) #must be a list - if only one spectrum, remember to wrap it in list()
-###write the data into a new formate file
-writeMSData(peak_data_list, file.path(getwd(), "Ch6", "out.mz"), header = header_info, outformat = "mzml", rtime_seconds = TRUE ) #can import mzml mzxml and mzdata and export mzml and mzxml
